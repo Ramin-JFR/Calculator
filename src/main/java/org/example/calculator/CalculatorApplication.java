a7f95992e99f633413c3ed076208548d47f526b0
raminjafari <ramin.jfr@yahoo.com>
2024-03-12 21:29:28 +0100

    -make it wrong

--- src/main/java/org/example/calculator/CalculatorApplication.java
+++ src/main/java/org/example/calculator/CalculatorApplication.java
@@ -7,7 +7,7 @@ public class CalculatorApplication {
     }
 
     public double add(double a, double b) {
-        return a + b;
+        return a - b;
     }
 
     public double sub(double a, double b) {
